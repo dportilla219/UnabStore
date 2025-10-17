bd0d36b446f56a44e6c2d488e9d77e2d75e1a309
David Eduardo Guerrero Portilla <davidportilla219@gmail.com>
2025-10-17 11:56:01 -0500

    Firebase auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,4 +60,8 @@ dependencies {
 
     //Navigation
     implementation(libs.androidx.navigation.compose)
+    //firebase
+    implementation(libs.firebase.bom)
+    //Auth
+    implementation(libs.firebase.auth)
 }
